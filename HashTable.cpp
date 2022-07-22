3730613f5d8b2470d2b3edb932ec2205d9d0436f
Chris Richards <crichards999@gmail.com>
2022-07-21 22:44:53 -0500

    Initial Commit

---
+++ HashTable.cpp
@@ -0,0 +1,418 @@
+//============================================================================
+// Name        : HashTable.cpp
+// Author      : John Watson
+// Version     : 1.0
+// Copyright   : Copyright © 2017 SNHU COCE
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <string> // atoi
+#include <time.h>
+
+#include "CSVparser.hpp"
+
+using namespace std;
+
+//============================================================================
+// Global definitions visible to all methods and classes
+//============================================================================
+
+const unsigned int DEFAULT_SIZE = 179;
+
+// forward declarations
+double strToDouble(string str, char ch);
+
+// define a structure to hold bid information
+struct Bid {
+    string bidId; // unique identifier
+    string title;
+    string fund;
+    double amount;
+    Bid() {
+        amount = 0.0;
+    }
+};
+
+//============================================================================
+// Hash Table class definition
+//============================================================================
+
+/**
+ * Define a class containing data members and methods to
+ * implement a hash table with chaining.
+ */
+class HashTable {
+
+private:
+    // Define structures to hold bids
+    struct Node {
+        Bid bid;
+        unsigned int key;
+        Node *next;
+
+        // default constructor
+        Node() {
+            key = UINT_MAX;
+            next = nullptr;
+        }
+
+        // initialize with a bid
+        Node(Bid aBid) : Node() {
+            bid = aBid;
+        }
+
+        // initialize with a bid and a key
+        Node(Bid aBid, unsigned int aKey) : Node(aBid) {
+            key = aKey;
+        }
+    };
+
+    // Changed the vector type to be a vector of node pointers. This is for a hash table with chaining
+    vector<Node*> nodes;
+
+    unsigned int tableSize = DEFAULT_SIZE;
+
+    unsigned int hash(int key);
+
+public:
+    HashTable();
+    HashTable(unsigned int size);
+    virtual ~HashTable();
+    void Insert(Bid bid);
+    void PrintAll();
+    void Remove(string bidId);
+    Bid Search(string bidId);
+};
+
+/**
+ * Default constructor
+ */
+HashTable::HashTable() {
+    // Initalize node structure by resizing tableSize
+    this->nodes.resize(this->tableSize);
+}
+
+/**
+ * Constructor for specifying size of the table
+ * Use to improve efficiency of hashing algorithm
+ * by reducing collisions without wasting memory.
+ */
+HashTable::HashTable(unsigned int size) {
+    // invoke local tableSize to size with this->
+    // resize nodes size
+    this->tableSize = size;
+    this->nodes.resize(this->tableSize);
+}
+
+
+/**
+ * Destructor
+ */
+HashTable::~HashTable() {
+    // FIXME (2): Implement logic to free storage when class is destroyed
+    Node *current;
+    Node *temp;
+    // iterate over the vector<Node> and removed all linked nodes in each bucket.
+    for (int i = 0; i < this->tableSize; ++i) {
+        current = this->nodes.at(i);
+        while (current != nullptr) {
+            temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+}
+
+/**
+ * Calculate the hash value of a given key.
+ * Note that key is specifically defined as
+ * unsigned int to prevent undefined results
+ * of a negative list index.
+ *
+ * @param key The key to hash
+ * @return The calculated hash
+ */
+unsigned int HashTable::hash(int key) {
+    // FIXME (3): Implement logic to calculate a hash value
+    // return key tableSize
+    return key % this->tableSize;
+}
+
+/**
+ * Insert a bid
+ *
+ * @param bid The bid to insert
+ */
+void HashTable::Insert(Bid bid) {
+    // FIXME (5): Implement logic to insert a bid
+    // create the key for the given bid
+    int key = this->hash(atoi(bid.bidId.c_str()));
+    // retrieve node using key
+    Node* node = this->nodes.at(key);
+    // if no entry found for the key
+    if (node == nullptr) {
+        // assign this node to the key position
+        node = new Node(bid, key);
+        this->nodes.at(key) = node;
+    }
+    else {
+        // else find the next open node
+        Node* current = node;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        // add new newNode to end
+        Node* newNode = new Node(bid, key);
+        current->next = newNode;
+    }  
+}
+
+/**
+ * Print all bids
+ */
+void HashTable::PrintAll() {
+    // FIXME (6): Implement logic to print all bids
+    // iterate through vector<node>
+    Node* current;
+    for (int i = 0; i < this->tableSize; ++i) {
+        // get the first node in the bucket
+        current = this->nodes.at(i);
+        while (current != nullptr) {
+            // iterate through each node and print it
+            cout << current->bid.bidId << ": " << current->bid.title << " | " << current->bid.amount << " | " << current->bid.fund << endl;
+            current = current->next;
+        }
+    }
+
+}
+
+/**
+ * Remove a bid
+ *
+ * @param bidId The bid id to search for
+ */
+void HashTable::Remove(string bidId) {
+    // FIXME (7): Implement logic to remove a bid
+    // set key equal to hash atoi bidID cstring
+    Node* current;
+    Node* temp;
+    int key = this->hash(atoi(bidId.c_str()));
+    // set current node to node at key
+    current = this->nodes.at(key);
+    
+    // if the node is null, no bid to delete
+    if (current == nullptr) {
+        return;
+    }
+
+    // if the first node is the bid to delete
+    if (current->bid.bidId == bidId) {
+        // create a temp, set the next node to the current node's next and delete the temp node
+        temp = current;
+        current = current->next;
+        delete temp;
+        // assign the new next node to the node at the key
+        this->nodes.at(key) = current;
+        return;
+    }
+
+    // If the first node at the key is not the one to delete
+    while (current->next != nullptr) {
+        // iterate through nodes until we fine the one to delete.
+        if (current->next->bid.bidId == bidId) {
+            temp = current->next;
+            current->next = current->next->next;
+            delete temp;
+            return;
+        }
+        current = current->next;
+    }
+    // erase node begin and key
+}
+
+/**
+ * Search for the specified bidId
+ *
+ * @param bidId The bid id to search for
+ */
+Bid HashTable::Search(string bidId) {
+    Bid bid;
+
+    // FIXME (8): Implement logic to search for and return a bid
+    Node* current;
+    // create the key for the given bid
+    int key = this->hash(atoi(bidId.c_str()));
+    // set current node to the node at the key
+    current = this->nodes.at(key);
+    // iterate through the nodes
+    while (current != nullptr) {
+        // if the node contains the bidId we are looking for, return it
+        if (current->bid.bidId == bidId) {
+            return current->bid;
+        }
+        // get next node
+        current = current->next;
+    }
+    // bid not found, return empty bid
+    return bid;
+}
+
+//============================================================================
+// Static methods used for testing
+//============================================================================
+
+/**
+ * Display the bid information to the console (std::out)
+ *
+ * @param bid struct containing the bid info
+ */
+void displayBid(Bid bid) {
+    cout << bid.bidId << ": " << bid.title << " | " << bid.amount << " | "
+            << bid.fund << endl;
+    return;
+}
+
+/**
+ * Load a CSV file containing bids into a container
+ *
+ * @param csvPath the path to the CSV file to load
+ * @return a container holding all the bids read
+ */
+void loadBids(string csvPath, HashTable* hashTable) {
+    cout << "Loading CSV file " << csvPath << endl;
+
+    // initialize the CSV Parser using the given path
+    csv::Parser file = csv::Parser(csvPath);
+
+    // read and display header row - optional
+    vector<string> header = file.getHeader();
+    for (auto const& c : header) {
+        cout << c << " | ";
+    }
+    cout << "" << endl;
+
+    try {
+        // loop to read rows of a CSV file
+        for (unsigned int i = 0; i < file.rowCount(); i++) {
+
+            // Create a data structure and add to the collection of bids
+            Bid bid;
+            bid.bidId = file[i][1];
+            bid.title = file[i][0];
+            bid.fund = file[i][8];
+            bid.amount = strToDouble(file[i][4], '$');
+
+            //cout << "Item: " << bid.title << ", Fund: " << bid.fund << ", Amount: " << bid.amount << endl;
+
+            // push this bid to the end
+            hashTable->Insert(bid);
+        }
+    } catch (csv::Error &e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+/**
+ * Simple C function to convert a string to a double
+ * after stripping out unwanted char
+ *
+ * credit: http://stackoverflow.com/a/24875936
+ *
+ * @param ch The character to strip out
+ */
+double strToDouble(string str, char ch) {
+    str.erase(remove(str.begin(), str.end(), ch), str.end());
+    return atof(str.c_str());
+}
+
+/**
+ * The one and only main() method
+ */
+int main(int argc, char* argv[]) {
+
+    // process command line arguments
+    string csvPath, bidKey;
+    switch (argc) {
+    case 2:
+        csvPath = argv[1];
+        bidKey = "98109";
+        break;
+    case 3:
+        csvPath = argv[1];
+        bidKey = argv[2];
+        break;
+    default:
+        csvPath = "eBid_Monthly_Sales_Dec_2016.csv";
+        bidKey = "98109";
+    }
+
+    // Define a timer variable
+    clock_t ticks;
+
+    // Define a hash table to hold all the bids
+    HashTable* bidTable;
+
+    Bid bid;
+    bidTable = new HashTable();
+    
+    int choice = 0;
+    while (choice != 9) {
+        cout << "Menu:" << endl;
+        cout << "  1. Load Bids" << endl;
+        cout << "  2. Display All Bids" << endl;
+        cout << "  3. Find Bid" << endl;
+        cout << "  4. Remove Bid" << endl;
+        cout << "  9. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+
+        case 1:
+            
+            // Initialize a timer variable before loading bids
+            ticks = clock();
+
+            // Complete the method call to load the bids
+            loadBids(csvPath, bidTable);
+
+            // Calculate elapsed time and display result
+            ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+            cout << "time: " << ticks << " clock ticks" << endl;
+            cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+            break;
+
+        case 2:
+            bidTable->PrintAll();
+            break;
+
+        case 3:
+            ticks = clock();
+
+            bid = bidTable->Search(bidKey);
+
+            ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+
+            if (!bid.bidId.empty()) {
+                displayBid(bid);
+            } else {
+                cout << "Bid Id " << bidKey << " not found." << endl;
+            }
+
+            cout << "time: " << ticks << " clock ticks" << endl;
+            cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+            break;
+
+        case 4:
+            bidTable->Remove(bidKey);
+            break;
+        }
+    }
+
+    cout << "Good bye." << endl;
+
+    return 0;
+}
